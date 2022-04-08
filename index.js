aaefaf575aff996b05dbcb5f2c08e8791b64651b
MayankArya123 <mayanksunny786@gmail.com>
2022-04-08 14:58:54 +0530

    cors-error-resolved

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const express = require('express')
+const cors = require('cors')
 const mongoose  = require('mongoose')
-const letter  = require('./models/letter')
+
 
 const {PORT,MONGO_URI} = require('./Config/keys')
 
@@ -14,216 +15,16 @@ mongoose.connect(MONGO_URI).then((succs)=>console.log('mongodb connection connec
 
 
 
+
+app.use(cors())
 app.use(express.json())
 
-app.get('/alphabets', async  ( req,res)=>{
 
-    console.log('route hitting')
 
-    try{
-        const alphabets = await letter.find({})
 
-        console.log('alphabets',alphabets)
 
-      res.send(alphabets)
+app.use('',require('./Routes/home'))
 
-    }catch(err){
-
-        console.log('error',err)
-
-        res.send(err)
-
-    }
-
-})
-
-
-
-
-app.post('/solve_equation', async  ( req,res)=>{
-
-    // console.log('route hitting')
-
-    const {value1,value2,operationSign,compareSign,compareResult} = req.body
-
-      console.log(value1,value2,operationSign,compareSign,compareResult)
-
-      if(!value1  || !value2   || !operationSign || !compareSign || !compareResult === (0 && !null ) )  {
-
-
-      return  res.status(401).send({
-            msg:'wrong wquation',
-            succs:false
-        })
-
-      }
-
-      
-
-     var operation_result = null
-
-      if(operationSign === '+'){
-
-        operation_result = value1 + value2
-
-      }
-      if(operationSign === '-'){
-
-    operation_result = value1 - value2
-
-      }
-      if(operationSign === '*'){
-
-    operation_result = value1 * value2
-
-      }
-      if(operationSign === '/'){
-
-    operation_result = value1 / value2
-
-      }
-
-
-      console.log('seeing',operation_result)
-
-
-
-
-
-if(compareSign === '>') {
-
-    if(operation_result > compareResult){
-
-        console.log('true is greater')
-
-
-         res.send({
-            succs:true,
-            message:'is greater',
-            operation_result:operation_result
-        })
-
-    }
-
-    else {
-
-        console.log('oo',operation_result)
-
-        console.log('false not greater')
-
-        
-        res.send({
-            succs:false,
-            message:'is not greater',
-            operation_result:operation_result
-        })
-
-    }
-
-}
-
-else{
-    
-if(compareSign === '<') {
-
-
-    console.log( 'resultss',operation_result)
-
-    if(operation_result < compareResult){
-
-        console.log('true is smaller')
-
-        res.send({
-            succs:true,
-            message:'is smaller',
-            operation_result:operation_result
-        })
-
-    }
-
-    else {
-
-        console.log('false not smaller')
-
-        res.send({
-            succs:false,
-            message:'is not smaller',
-            operation_result:operation_result
-        })
-
-    }
-
-}
-
-
-}
-
-    // try{
-    //     // const alphabets = await letter.find({})
-
-    //     // console.log('alphabets',alphabets)
-
-    // //   res.send(alphabets)
-
-    // }catch(err){
-
-    //     console.log('error',err)
-
-    //     res.send(err)
-
-    // }
-
-})
-
-
-// app.post('/insertMany', async  ( req,res)=>{
-
-//     console.log('route hitting')
-
-//     try{
-
-
-//         const letters = [
-            
-//             {
-//                letter:'A',
-//                value:1
-//             },
-//             {
-//                letter:'B',
-//                value:2
-//             },
-//             {
-//                letter:'C',
-//                value:3
-//             },
-//             {
-//                letter:'D',
-//                value:4
-//             },
-//             {
-//                letter:'E',
-//                value:5
-//             }
-
-
-//         ]
-
-//         const alphabets = await letter.insertMany(letters)
-
-//         console.log('alphabets',alphabets)
-
-//       res.send(alphabets)
-
-//     }catch(err){
-
-//         console.log('error',err)
-
-//         res.send(err)
-
-//     }
-
-// })
 
 
 
